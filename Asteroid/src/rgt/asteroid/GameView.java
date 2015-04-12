d396125186d15cff2e7178bf84532bc567446968
RRC97 <matheuspereira.nave@gmail.com>
2015-04-12 14:30:51 -0300

    BeginProject

---
+++ Asteroid/src/rgt/asteroid/GameView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rgt.asteroid;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ *
+ * @author Casa
+ */
+public class GameView extends View
+{
+    public GameView(Context context)
+    {
+        super(context);
+    }
+    
+}
